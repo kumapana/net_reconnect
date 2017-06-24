52610576f31a455e71ca66310008a6c3d1f84902
panakuma <kumachan05-11@hotmail.co.jp>
2017-06-25 01:26:31 +0900

    試行回数によって処理を変える実装完了

--- reconnect.bat
+++ reconnect.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 rem ゲートウェイIP設定
 set gateway_ip 10.203.10.1
@@ -7,8 +8,10 @@ rem ネットワークチップのデバイスIDの設定
 set device_id "device id"
 
 rem ログファイルのパスの設定
-set log_path C:\Users\Linuxclub\Desktop\net_test.log
+set log_path C:\Users\Linuxclub\Desktop\net_test_log.csv
 
+rem 実行回数を保存する変数宣言
+set /a count=0
 
 rem デフォルトゲートウェイにPINGを実行し疎通試験。
 :TEST
@@ -16,21 +19,31 @@ ping gateway_ip -n 5
 if errorlevel 0 GOTO OK
 if errorlevel 1 GOTO NG
 
-rem 疎通試験合格の場合
+rem 疎通試験合格の場合ログを吐いて終了
 :OK
-echo %date% %time% OK>> log_path
+echo %date%,%time%,!count!,OK>> log_path
 GOTO END
 
 rem 疏通試験失敗の場合
-:NG 
-echo %date% %time% NG >> log_path
+:NG
+rem 試行回数が5回未満の時はリセット実行
+if count lss 5 GOTO RESET
+
+rem 試行回数が5の時は終了
+if count equ 5 GOTO END
+
+:RESET
+echo %date%,%time%,!count!,NG >> log_path
 rem ネットワークチップの無効化
 devcon disable "device_id"
 ping 127.0.0.1 -n 15
 
 rem ネットワークチップの有効化
-devcon enable "device_id"
+devcon enable device_id
 ping 127.0.0.1 -n 15
+
+rem チップリセット回数の記録
+set /a count=count+1
 
 rem 再度疎通試験
 GOTO TEST
